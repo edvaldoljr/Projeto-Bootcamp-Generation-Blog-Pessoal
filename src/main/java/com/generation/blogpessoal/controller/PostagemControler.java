271980bdb524e1da46b9b6c31ff29d77a5ef7f82
edvaldoljr <edvaldoluizj@gmail.com>
2024-07-05 19:48:04 -0300

    projeto blog pessoal

--- src/main/java/com/generation/blogpessoal/controller/PostagemControler.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemControler.java
@@ -1,11 +1,10 @@
-package com.generation.blogPessoal.controller;
+package com.generation.blogpessoal.controller;
 
-import com.generation.blogPessoal.model.Postagem;
-import com.generation.blogPessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
---
+++ src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.blogpessoal.repository;
+
+import com.generation.blogpessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+}

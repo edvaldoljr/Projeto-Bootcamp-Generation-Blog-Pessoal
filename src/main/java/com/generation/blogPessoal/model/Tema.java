103d673db5b69673eee7df4eac179b25d83b3ad1
edvaldoljr <edvaldoluizj@gmail.com>
2024-07-05 18:53:43 -0300

    Classe tema

--- src/main/java/com/generation/blogPessoal/model/Tema.java
+++ src/main/java/com/generation/blogPessoal/model/Tema.java
@@ -1,4 +1,33 @@
 package com.generation.blogPessoal.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas")
 public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O campo Descrição é obrigatorio")
+    private String descriao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotNull(message = "O campo Descrição é obrigatorio") String getDescriao() {
+        return descriao;
+    }
+
+    public void setDescriao(@NotNull(message = "O campo Descrição é obrigatorio") String descriao) {
+        this.descriao = descriao;
+    }
 }

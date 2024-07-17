16e07364343e9eb025b9f1f1c3e14bc1611b1775
edvaldoljr <edvaldoluizj@gmail.com>
2024-07-16 23:29:17 -0300

    Spring Security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -41,6 +41,10 @@ public class Postagem {
     @JsonIgnoreProperties("postagem")
     private Tema tema;
 
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
+
     public Long getId() {
         return id;
     }
@@ -81,4 +85,11 @@ public class Postagem {
         this.tema = tema;
     }
 
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
